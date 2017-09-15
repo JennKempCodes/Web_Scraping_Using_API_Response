79598a059a2ef823b14aa86211188388828ee1ee
Jennifer Kemp <jenn.kemp.codes@gmail.com>
2017-09-15 17:10:11 -0400

    Added Load time to success email with minor refactoring

--- web_page_test_scrape1.rb
+++ web_page_test_scrape1.rb
@@ -4,15 +4,10 @@ require 'xpath'
 require 'crack'
 require 'gmail'
 
-# @parse_page = []
-# [] = Array.new
-@status_return = 'statusCode'
-# Emails may block sign-in attempts from this script. You can allow access by this script by "allowing access to less secure apps"
-
 @password = 'INSERT PASSWORD'
 @username = 'INSERT EMAIL'
 
-def send_email(status)
+def send_email(status,loadTime)
   @gmail = Gmail.connect(@username, @password)
 
   email = @gmail.compose do
@@ -20,7 +15,7 @@ def send_email(status)
 
     if status.to_i == 200
      subject 'Your website is up and running!'
-      body 'We have succesfully pinged your website and it is up and working'
+      body 'We have succesfully pinged your website and it is up and working with an average loadtime of ' + loadTime + 'ms'
       puts 'Success email has been sent'
     else
       subject 'Your website is not up working'
@@ -39,6 +34,8 @@ end
 
 @status_code_response = @parsed_xml['response']['statusCode']
 
+@page_load_time = @parsed_xml ['response']['data']['average']['firstView']['loadTime']
+
 # This is where I wait for the URL to finish testing
 until @status_code_response.to_i == 200
   puts "Waiting for 200"
@@ -52,4 +49,5 @@ else
   puts "still no 200...gonna' sleep for a little while :-)"
 end
 
-send_email(@status_code_response)
+send_email(@status_code_response, @page_load_time)
+# Gmail may block sign-in attempts from this script. You can allow access by this script by "allowing access to less secure apps"
