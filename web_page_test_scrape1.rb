9ae9b5f1e0d56433320c8393701025eebb86d034
Jennifer Kemp <jenn.kemp.codes@gmail.com>
2017-09-15 16:36:48 -0400

    Working Script

--- web_page_test_scrape1.rb
+++ web_page_test_scrape1.rb
@@ -1,34 +1,36 @@
-#require 'nokogiri'
 require 'open-uri'
 require 'HTTParty'
 require 'xpath'
 require 'crack'
+require 'gmail'
 
 # @parse_page = []
 # [] = Array.new
 @status_return = 'statusCode'
+# Emails may block sign-in attempts from this script. You can allow access by this script by "allowing access to less secure apps"
 
-@password = 'PASSWORD'
-@username = 'EMAIL'
+@password = 'INSERT PASSWORD'
+@username = 'INSERT EMAIL'
 
-# Read in a URL from a file
+def send_email(status)
+  @gmail = Gmail.connect(@username, @password)
 
+  email = @gmail.compose do
+    to 'Jenn.Kemp.Codes@gmail.com'
 
+    if status.to_i == 200
+     subject 'Your website is up and running!'
+      body 'We have succesfully pinged your website and it is up and working'
+      puts 'Success email has been sent'
+    else
+      subject 'Your website is not up working'
+      body 'We have pinged your website and it is not functioning properly'
+      puts 'Failure email has been sent'
+      end
+  end
+  email.deliver!
+end
 
-
-# @file_url = readfromfile
-
-#input URL into test box
-
-# click the start button
-
-# grab the url call original_url
-
-# now change it to the XML api version - call it xml_url
-
-# use ruby .gsub to remove the /result/ and change it to xmlResult
-
-  #  @file_url     # http://www.webpagetest.org/result/170908_XW_6863b2c6c39527238e4b2f9146c6f78b/
 @web_page_test_loading_results_url = 'http://www.webpagetest.org/xmlResult/170904_JM_db94ee89b80bac884269b9b857fab766/'
 
 @page = HTTParty.get(@web_page_test_loading_results_url).to_s
@@ -44,16 +46,10 @@ until @status_code_response.to_i == 200
   end
 
 if @status_code_response.to_i == 200
-  puts "Hey Josh isn't this awesome!!! " + @status_code_response
+  puts "Hey isn't this awesome!!! " + @status_code_response
 
 else
   puts "still no 200...gonna' sleep for a little while :-)"
 end
 
-#begin parse for value #loadTime
-
-
-
- # @parse_page.get('loadTime').text
-
-#begin email send
+send_email(@status_code_response)
